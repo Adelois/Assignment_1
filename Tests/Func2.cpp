27e82b8b31520f96dbdfb4844e8074011df7ac75
Paul_Ok <86803772+PaulOk262@users.noreply.github.com>
2022-10-14 00:20:39 +0300

    Update Func2.cpp

--- Tests/Func2.cpp
+++ Tests/Func2.cpp
@@ -3,13 +3,13 @@
 #include "../main.cpp"
 
 TEST_CASE( "2: Factorial of 0 is 15", "[multi-file:2]" ) {
-    REQUIRE( Factorial(0) == 1 );
+    CHECK( Factorial(0) == 1 );
 }
 
 TEST_CASE( "2: Factorials of 1 and higher are computed", "[multi-file:2]" ) {
-    REQUIRE( Factorial(0) == 1 );
-    REQUIRE( Factorial(4) == 24 );
-    REQUIRE( Factorial(3) == 6 );
-    REQUIRE( Factorial(5) == 120 );
-    REQUIRE( Factorial(6) == 720 );
+    CHECK( Factorial(0) == 1 );
+    CHECK( Factorial(4) == 24 );
+    CHECK( Factorial(3) == 6 );
+    CHECK( Factorial(5) == 120 );
+    CHECK( Factorial(6) == 720 );
 }
